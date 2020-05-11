1596b10bc0a930fe6dc2acebc3d781d97d882a1a
Muhammad Tifak <moht20009@gmail.com>
2020-05-10 23:38:49 -0700

    Completed contains + find_index

--- Src-Code/strings.py
+++ Src-Code/strings.py
@@ -5,14 +5,33 @@ def contains(text, pattern):
     assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
     # TODO: Implement contains here (iteratively and/or recursively)
+    if find_all_indexes(text, pattern) or pattern == '':
+        return True
+    else:
+        return False
 
 
-def find_index(text, pattern):
+def find_index(text, pattern, index=0):
     """Return the starting index of the first occurrence of pattern in text,
     or None if not found."""
     assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
     # TODO: Implement find_index here (iteratively and/or recursively)
+    if pattern == '':
+        return index
+    
+    while index < len(text):
+        if text[index]  == pattern[0]:
+            ind = index
+            for letter in pattern:
+                if ind > len(text) - 1 or text[ind] != letter:
+                    break
+                ind += 1
+            else:
+                return index
+
+        index += 1
+    return None
 
 
 def find_all_indexes(text, pattern):
